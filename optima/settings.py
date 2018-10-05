4b42062973e87228f76d09789b26319e47fa9dc9
zeerorg <r.g.gupta@outlook.com>
2018-10-05 17:28:20 +0530

    Added cors header. Allowed all for testing period till we finalize domain

--- optima/settings.py
+++ optima/settings.py
@@ -136,3 +136,5 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CORS_ORIGIN_ALLOW_ALL = True
